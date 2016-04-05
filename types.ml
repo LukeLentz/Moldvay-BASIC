2fcf6f17b892ce0645ad0d86dc9a558a9555311f
LukeLentz <lentzl17@gmail.com>
2016-04-05 18:36:58 -0400

    made some types

---
+++ types.ml
@@ -0,0 +1,5 @@
+type exprC = IntC of int | FloatC of float | BoolC of bool | IfC of (exprC * exprC * exprC)
+
+type exprS = IntS of int | FloatS of float | BoolS of bool | IfS of (exprS * exprS * exprS) | OrS of (exprS * exprS) | AndS of (exprS * exprS) | XOrS of (exprS * exprS) | NotS of (exprS)
+
+type value = Int of int | Float of float | Bool of bool
