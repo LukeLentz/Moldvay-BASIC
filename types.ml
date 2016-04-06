5f88ca6f83d7fcd283e24dffadcd756c0fddfb94
LukeLentz <lentzl17@gmail.com>
2016-04-05 21:04:39 -0400

    more types

--- types.ml
+++ types.ml
@@ -1,5 +1,5 @@
-type exprC = IntC of int | FloatC of float | BoolC of bool | IfC of (exprC * exprC * exprC)
+type exprC = IntC of int | FloatC of float | BoolC of bool | IfC of (exprC * exprC * exprC) | ArithC of (exprC * exprC * exprC)
 
-type exprS = IntS of int | FloatS of float | BoolS of bool | IfS of (exprS * exprS * exprS) | OrS of (exprS * exprS) | AndS of (exprS * exprS) | XOrS of (exprS * exprS) | NotS of (exprS)
+type exprS = IntS of int | FloatS of float | BoolS of bool | IfS of (exprS * exprS * exprS) | OrS of (exprS * exprS) | AndS of (exprS * exprS) | XOrS of (exprS * exprS) | NotS of (exprS) | ArithS of (exprS   * exprS * exprS)
 
 type value = Int of int | Float of float | Bool of bool
