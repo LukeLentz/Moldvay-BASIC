7cc6b708f334f12164b0876a85a1084bdfdfba5a
Flynn Horrell <horrellf16@hanover.edu>
2016-04-07 18:33:33 -0400

    Copied over some code from assignment 8

---
+++ types.mli
@@ -0,0 +1,38 @@
+type exprC = IntC of int 
+                      | FloatC of float 
+                      | BoolC of bool 
+                      | IfC of (exprC * exprC * exprC) 
+                      | ArithC of (exprC * exprC * exprC) 
+                      | IfC of (exprC * exprC * exprC) 
+                      | CompC of (exprC * exprC * exprC) 
+                      | EqC of (exprC * exprC)
+
+type exprS = IntS of int 
+                      | FloatS of float | BoolS of bool 
+                      | IfS of (exprS * exprS * exprS) 
+                      | OrS of (exprS * exprS) 
+                      | AndS of (exprS * exprS) 
+                      | XOrS of (exprS * exprS)
+                      | NAndS of (exprS * exprS)
+                      | NotS of (exprS) 
+                      | ArithS of (exprS   * exprS * exprS) 
+                      | IfS of (exprS * exprS * exprS) 
+                      | CompS of (exprS * exprS * exprS) 
+                      | EqS of (exprS * exprS) 
+                      | NeqS of (exprS * exprS)
+
+type value = Int of int | Float of float | Bool of bool
+
+(* Environment lookup *)
+type 'a env
+val empty : 'a env
+val lookup : string -> 'a env -> 'a option
+val bind :  string -> 'a -> 'a env -> 'a env
+
+(* Interpreter steps *)
+val desugar : exprS -> exprC
+val interp : value env -> exprC -> value
+val evaluate : exprC -> value
+
+(* result post-processing *)
+val valToString : value -> string
