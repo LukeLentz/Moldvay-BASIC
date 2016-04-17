4e0dd35be1e84f70be3a0647fc40c2f7c5ace9a8
Flynn Horrell <horrellf16@hanover.edu>
2016-04-17 18:26:32 -0400

    Added test for tuple

--- tests.ml
+++ tests.ml
@@ -1,7 +1,7 @@
 open Types
 
-let t0a = evaluate (IntC 2) = Int 2
-let t0a = evaluate (FloatC 2.3) = Float 2.3
+let t0a = evaluate (IntC 2) = (IntT,Int 2)
+(*let t0a = evaluate (FloatC 2.3) = Float 2.3
 let t0c = desugar (IntS 2) = IntC 2
 let t0c = desugar (FloatS 2.3) = FloatC 2.3
 
@@ -48,3 +48,7 @@ let t5d = evaluate (EqC (BoolC true, BoolC true)) = Bool true
 let t5e = evaluate (EqC (FloatC 20., BoolC true)) = Bool false
 let t5f = desugar  (EqS (FloatS 20., BoolS true)) = EqC (FloatC 20., BoolC true)
 let t5g = desugar  (NeqS (FloatS 20., BoolS true)) =  IfC (EqC (FloatC 20., BoolC true), BoolC false, BoolC true) 
+*)
+
+let t6a = desugar (TupleS (FloatS 2.,IntS 5)) = (TupleC (FloatC 2.,IntC 5))
+let t6b = evaluate (desugar (TupleS (FloatS 1.,BoolS true))) = (TupleT, Tuple (Float 1.,Bool true))
